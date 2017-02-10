8b90ce907fe26f420af2b696adc5b24f7b3a227c
Devendra Kumar Gadde <gaddedev@gmail.com>
2017-02-10 21:16:00 +0530

    Create indxeq.c

---
+++ indxeq.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int ind(int* a, int n){
+	int i;
+	for(i=0;i<n;i++)
+	{
+  	if(i==a[i])
+  	return i;
+  }
+}
+int main()
+{
+  int i,n,t;
+  int* a;
+  scanf("%d",&n);
+  if(n<0)
+  return -1;
+  a=(int*)malloc(sizeof(int)*n);
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  t=ind(a,n);
+  return t;
+}
